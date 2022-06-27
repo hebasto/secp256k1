d98be43d08fcfaef756309bb3048a57274b0ccb6
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2022-06-27 16:53:24 +0200

    build: Add CMake-based build system

---
+++ toolchain/x86_64-w64-mingw32.cmake
@@ -0,0 +1,7 @@
+# Copyright 2022
+# Distributed under the MIT software license, see the accompanying
+# file COPYING or https://www.opensource.org/licenses/mit-license.php.
+
+set(CMAKE_SYSTEM_NAME Windows)
+set(CMAKE_SYSTEM_PROCESSOR x86_64)
+set(CMAKE_C_COMPILER x86_64-w64-mingw32-gcc)
