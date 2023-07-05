14abe94bd0cc7ab44c7fa078944fae3bcb207206
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2023-07-05 13:41:26 +0100

    refactor: Make `int128*.h` headers self-contained

--- src/int128_native_impl.h
+++ src/int128_native_impl.h
@@ -1,7 +1,7 @@
 #ifndef SECP256K1_INT128_NATIVE_IMPL_H
 #define SECP256K1_INT128_NATIVE_IMPL_H
 
-#include "int128.h"
+#include "int128_native.h"
 #include "util.h"
 
 static SECP256K1_INLINE void secp256k1_u128_load(secp256k1_uint128 *r, uint64_t hi, uint64_t lo) {
