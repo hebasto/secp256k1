225096f8bb41de12d88848148d734e38ac362a6c
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2025-06-04 20:00:18 +0100

    cmake: Fix `CheckX86_64Assembly` module
    This change fixes a `-Wuninitialized` warning triggered under clang-cl,
    which may lead to test failure when using the `/WX` flag.

--- cmake/CheckX86_64Assembly.cmake
+++ cmake/CheckX86_64Assembly.cmake
@@ -6,7 +6,7 @@ function(check_x86_64_assembly)
 
     int main()
     {
-      uint64_t a = 11, tmp;
+      uint64_t a = 11, tmp = 0;
       __asm__ __volatile__(\"movq $0x100000000,%1; mulq %%rsi\" : \"+a\"(a) : \"S\"(tmp) : \"cc\", \"%rdx\");
     }
   " HAVE_X86_64_ASM)
