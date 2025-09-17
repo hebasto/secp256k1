8b75fa0807f92713a98367eea51836d9251f9d32
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2025-09-17 23:19:12 +0100

    build: Fix warnings in x86_64 assembly check
    This change fixes:
    - `-Wuninitialized` in both Autotools and CMake;
    - `-Wreturn-type` in CMake only.

--- cmake/CheckX86_64Assembly.cmake
+++ cmake/CheckX86_64Assembly.cmake
@@ -6,8 +6,9 @@ function(check_x86_64_assembly)
 
     int main()
     {
-      uint64_t a = 11, tmp;
+      uint64_t a = 11, tmp = 0;
       __asm__ __volatile__(\"movq $0x100000000,%1; mulq %%rsi\" : \"+a\"(a) : \"S\"(tmp) : \"cc\", \"%rdx\");
+      return 0;
     }
   " HAVE_X86_64_ASM)
   set(HAVE_X86_64_ASM ${HAVE_X86_64_ASM} PARENT_SCOPE)
